9adeacc3600bb1a2e1a22d9caa05a9316b631822
Saptarshi <saptarshimemari072@gmail.com>
2023-05-20 04:38:05 +0530

    Contact Page into the arena

---
+++ public/assets/js/contact.js
@@ -0,0 +1,35 @@
+console.log("Hello Contact");
+
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = ()=>{
+    scrollFunction();
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTo({
+    top: 0,
+    behaviour: "smooth",
+  });
+}
+
+function scrollFunction() {
+    // console.log(document.documentElement.scrollTop);
+    if (
+      document.body.scrollTop > 100 ||
+      document.documentElement.scrollTop > 100
+    ) {
+      document.getElementById("collapse").style.top = "0";
+      document.getElementById("collapse").style.position = "sticky";
+    } else {
+      document.getElementById("collapse").style.top = "-56px";
+      document.getElementById("collapse").style.position = "relative";
+    }
+
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+}
